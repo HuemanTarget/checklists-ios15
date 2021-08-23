492b468d0d7b568f9ff7e408eeec2afd0e79a834
Joshua Basche <pompouscat@Joshuas-MacBook-Air.local>
2021-08-23 16:43:24 -0700

    create textField and textFieldShouldClear delegate functions in AddItemControllerView

--- Checklists/Controllers/AddItemViewController.swift
+++ Checklists/Controllers/AddItemViewController.swift
@@ -7,16 +7,23 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
   
   // MARK: - Outlets
   @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+  
     navigationItem.largeTitleDisplayMode = .never
     
+  }
+  
+  // MARK: - Functions
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
   }
   
   // MARK: - Table View Delegates
@@ -30,7 +37,26 @@ class AddItemViewController: UITableViewController {
   }
   
   @IBAction func done() {
+    
+    print("Contents of the Text Field: \(textField.text!)")
+    
     navigationController?.popViewController(animated: true)
+  }
+  
+  // MARK: - Text Field Delegates
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    let oldText = textField.text!
+    let stringRange = Range(range, in: oldText)!
+    let newText = oldText.replacingCharacters(in: stringRange, with: string)
+    
+    doneBarButton.isEnabled = !newText.isEmpty
+    
+    return true
+  }
+  
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    doneBarButton.isEnabled = false
+    return true
   }
   
 }
