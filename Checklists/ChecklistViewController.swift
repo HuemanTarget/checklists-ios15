ae83f5d88ce1c4df2bada0e2a6f90ad793d14a86
Joshua Basche <joshbasche@live.com>
2021-08-23 11:54:59 -0700

    create Protype Cell in Table View

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -13,6 +13,19 @@ class ChecklistViewController: UITableViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+  
+  
+  // MARK: - Table View Number Of Rows
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  // MARK: - Table View Data Source
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+    
+    return cell
+  }
 
 
 }
