f13bfbd2d673c915be1fbdcd6ab1e1ddf0fc370d
Joshua Basche <joshbasche@live.com>
2021-08-23 13:48:08 -0700

    create ChecklistItem Model

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  CheckListItem.swift
+//  CheckListItem
+//
+//  Created by Joshua Basche on 8/23/21.
+//
+
+import Foundation
+
+class ChecklistItem {
+  var text: String = ""
+  var checked: Bool = false
+}
--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,43 +9,48 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
   // MARK: - Variables
-  let row0text = "Walk the dog"
-  let row1text = "Brush teeth"
-  let row2text = "Learn iOS development"
-  let row3text = "Soccer practice"
-  let row4text = "Eat ice cream"
+  var items = [ChecklistItem]()
   
-  var row0checked = false
-  var row1checked = false
-  var row2checked = false
-  var row3checked = false
-  var row4checked = false
+  var row0item = ChecklistItem()
+  var row1item = ChecklistItem()
+  var row2item = ChecklistItem()
+  var row3item = ChecklistItem()
+  var row4item = ChecklistItem()
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    let item1 = ChecklistItem()
+    item1.text = "Walk the dog"
+    items.append(item1)
+    
+    let item2 = ChecklistItem()
+    item2.text = "Brush my teeth"
+    item2.checked = true
+    items.append(item2)
+    
+    let item3 = ChecklistItem()
+    item3.text = "Learn iOS development"
+    item3.checked = true
+    items.append(item3)
+    
+    let item4 = ChecklistItem()
+    item4.text = "Soccer practice"
+    items.append(item4)
+    
+    let item5 = ChecklistItem()
+    item5.text = "Eat ice cream"
+    items.append(item5)
   }
   
   func configureCheckmark(
     for cell: UITableViewCell,
     at indexPath: IndexPath
   ) {
-    var isChecked = false
+    let item = items[indexPath.row]
     
-    if indexPath.row == 0 {
-      isChecked = row0checked
-    } else if indexPath.row == 1 {
-      isChecked = row1checked
-    } else if indexPath.row == 2 {
-      isChecked = row2checked
-    } else if indexPath.row == 3 {
-      isChecked = row3checked
-    } else if indexPath.row == 4 {
-      isChecked = row4checked
-    }
-    
-    if isChecked {
+    if item.checked {
       cell.accessoryType = .checkmark
     } else {
       cell.accessoryType = .none
@@ -62,9 +67,14 @@ class ChecklistViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
     
-//    let label = cell.viewWithTag(1000) as! UILabel
+    let item = items[indexPath.row]
+    
+    let label = cell.viewWithTag(1000) as! UILabel
+    
+    label.text = item.text
     
     configureCheckmark(for: cell, at: indexPath)
+    
     
     return cell
   }
@@ -72,29 +82,12 @@ class ChecklistViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
     if let cell = tableView.cellForRow(at: indexPath) {
-      var isChecked = false
       
-      if indexPath.row == 0 {
-        row0checked.toggle()
-        isChecked = row0checked
-      } else if indexPath.row == 1 {
-        row1checked.toggle()
-        isChecked = row1checked
-      } else if indexPath.row == 2 {
-        row2checked.toggle()
-        isChecked = row2checked
-      } else if indexPath.row == 3 {
-        row3checked.toggle()
-        isChecked = row3checked
-      } else if indexPath.row == 4 {
-        row4checked.toggle()
-        isChecked = row4checked
-      }
-      if isChecked {
-        cell.accessoryType = .checkmark
-      } else {
-        cell.accessoryType = .none
-      }
+      let item = items[indexPath.row]
+      item.checked.toggle()
+      
+      configureCheckmark(for: cell, at: indexPath)
+      
     }
     
     tableView.deselectRow(at: indexPath, animated: true)
