b865c9499f51f0358434be978eda7057e21cde7b
Joshua Basche <joshbasche@live.com>
2021-08-23 12:06:04 -0700

    create didSelectRowAt method in CheckListViewController

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
@@ -17,16 +17,43 @@ class ChecklistViewController: UITableViewController {
   
   // MARK: - Table View Number Of Rows
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return 5
   }
   
   // MARK: - Table View Data Source
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
     
+    let label = cell.viewWithTag(1000) as! UILabel
+    
+    if indexPath.row == 0 {
+      label.text = "Walk the dog"
+    } else if indexPath.row == 1 {
+      label.text = "Brush my teeth"
+    } else if indexPath.row == 2 {
+      label.text = "Learn iOS development"
+    } else if indexPath.row == 3 {
+      label.text = "Soccer practice"
+    } else if indexPath.row == 4 {
+      label.text = "Eat ice cream"
+    }
+    
     return cell
   }
-
-
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    if let cell = tableView.cellForRow(at: indexPath) {
+      if cell.accessoryType == .none {
+        cell.accessoryType = .checkmark
+      } else {
+        cell.accessoryType = .none
+      }
+    }
+    
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+  
+  
 }
 
